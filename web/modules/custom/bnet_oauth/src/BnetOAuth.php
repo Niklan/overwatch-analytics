c73645fda5b0a102b53a2e262e6b11c95ee05257
Niklan <niklanrus@gmail.com>
2017-12-13 20:53:26 +0500

    WIP

---
+++ web/modules/custom/bnet_oauth/src/BnetOAuth.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Drupal\bnet_oauth;
+
+use Drupal\Component\Render\FormattableMarkup;
+use Drupal\Core\Url;
+
+/**
+ * Class BnetOAuth.
+ */
+class BnetOAuth {
+
+  protected $clientId;
+
+  protected $secret;
+
+  protected $region = 'eu';
+
+  protected $authorizeUriPattern = 'https://@region.battle.net/oauth/authorize';
+
+  protected $authorizeUri;
+
+  protected $tokenUriPattern = 'https://@region.battle.net/oauth/token';
+
+  protected $tokenUri;
+
+  /**
+   * BnetOAuth constructor.
+   */
+  public function __construct() {
+    return $this;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getClientId() {
+    return $this->clientId;
+  }
+
+  /**
+   * @param mixed $clientId
+   */
+  public function setClientId($clientId) {
+    $this->clientId = $clientId;
+    return $this;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getSecret() {
+    return $this->secret;
+  }
+
+  /**
+   * @param mixed $secret
+   */
+  public function setSecret($secret) {
+    $this->secret = $secret;
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getRegion() {
+    return $this->region;
+  }
+
+  /**
+   * @param string $region
+   */
+  public function setRegion($region) {
+    $this->region = $region;
+    $this->authorizeUri = new FormattableMarkup($this->authorizeUriPattern, [
+      '@region' => $this->region,
+    ]);
+    $this->tokenUri = new FormattableMarkup($this->tokenUriPattern, [
+      '@region' => $this->region,
+    ]);
+    return $this;
+  }
+
+  /**
+   * Request Auth code from battle.net server.
+   */
+  public function getAuthorizationCodeUrl() {
+    $url = Url::fromUri($this->authorizeUri, [
+      'query' => [
+        'client_id' => $this->clientId,
+        'scope' => '',
+        'state' => $this->getCsrfToken(),
+        'redirect_uri' => 'https://overwatch-analytics.localhost/bnet/callback',
+        'response_type' => 'code',
+      ],
+    ]);
+    return $url->toString();
+  }
+
+  /**
+   * Generate token for state parameter of auth.
+   */
+  public function getCsrfToken() {
+    return \Drupal::csrfToken()->get(\Drupal::request()->getClientIp());
+  }
+
+  /**
+   * Compare Drupal generated csrf Token with provided.
+   *
+   * @param string $token
+   *   Token to compare.
+   *
+   * @return bool
+   *   TRUE if they are equal.
+   */
+  public function compareCsrfToken($token) {
+    return $token == $this->getCsrfToken();
+  }
+
+  /**
+   * Request Auth code from battle.net server.
+   */
+  public function getAccessTokenUrl($code) {
+    $url = Url::fromUri($this->tokenUri, [
+      'query' => [
+        'redirect_uri' => 'https://overwatch-analytics.localhost/',
+        'scope' => '',
+        'grant_type' => 'authorization_code',
+        'code' => $code,
+      ],
+    ]);
+
+    return $url->toString();
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getTokenUri() {
+    return $this->tokenUri;
+  }
+
+}
--- web/modules/custom/bnet_oauth/src/Controller/AuthController.php
+++ web/modules/custom/bnet_oauth/src/Controller/AuthController.php
@@ -2,9 +2,12 @@
 
 namespace Drupal\bnet_oauth\Controller;
 
+use Drupal\Component\Serialization\Json;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Routing\TrustedRedirectResponse;
 use Drupal\Core\Url;
+use SebastianBergmann\RecursionContext\Exception;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Class AuthController.
@@ -15,21 +18,53 @@ class AuthController extends ControllerBase {
    * {@inheritdoc}
    */
   public function auth() {
-    $region = 'eu';
+    \Drupal::service('page_cache_kill_switch')->trigger();
     $client_id = \Drupal::state()->get('bnet_oauth_settings_key');
-    $secret = \Drupal::state()->get('bnet_oauth_settings_secret');
 
-    $url = Url::fromUri('https://' . $region . '.battle.net/oauth/authorize', [
-      'query' => [
-        'client_id' => $client_id,
-        'scope' => '',
-        'state' => random_bytes(8),
-        'redirect_uri' => 'https://localhost',
-        'response_type' => 'code',
-      ],
-    ]);
+    /** @var \Drupal\bnet_oauth\BnetOAuth $bnet_oauth */
+    $bnet_auth_code_url = \Drupal::service('bnet_oauth')
+      ->setClientId($client_id)
+      ->setRegion('eu')
+      ->getAuthorizationCodeUrl();
 
-    return TrustedRedirectResponse::create($url->toString());
+    return TrustedRedirectResponse::create($bnet_auth_code_url);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function callback() {
+    $request_query = \Drupal::request()->query;
+    $state = $request_query->get('state');
+    /** @var \Drupal\bnet_oauth\BnetOAuth $bnet_oauth */
+    $bnet_oauth = \Drupal::service('bnet_oauth')->setRegion('eu');
+    $frontpage_url = Url::fromRoute('<front>')->toString();
+    if ($bnet_oauth->compareCsrfToken($state)) {
+      $response = \Drupal::httpClient()
+        ->post('https://eu.battle.net/oauth/token', [
+          'verify' => TRUE,
+          'form_params' => [
+            'client_id' => \Drupal::state()->get('bnet_oauth_settings_key'),
+            'client_secret' => \Drupal::state()
+              ->get('bnet_oauth_settings_secret'),
+            'redirect_uri' => 'https://overwatch-analytics.localhost/bnet/callback',
+            'scope' => '',
+            'grant_type' => 'authorization_code',
+            'code' => $request_query->get('code'),
+          ],
+        ]);
+      $response_result = Json::decode($response->getBody()->getContents());
+      if (!empty($response_result['access_token'])) {
+        $request = \Drupal::httpClient()
+          ->get('https://eu.api.battle.net/account/user?access_token=' . $response_result['access_token']);
+        ksm($request->getBody()->getContents());
+      }
+      return RedirectResponse::create($frontpage_url);
+    }
+    else {
+      drupal_set_message(t("Something wen't wrong during battle.net access."), 'error');
+      return RedirectResponse::create($frontpage_url);
+    }
   }
 
 }
