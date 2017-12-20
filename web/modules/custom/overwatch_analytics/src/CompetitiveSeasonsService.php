4bc207f82d52cabe0f5d1e85f1ae99237d4b9eb9
Niklan <niklanrus@gmail.com>
2017-12-20 10:59:33 +0500

    - Added season statistic service with basic summary data calculation. - Added hook to automate match status value. WIP - Some improvements in services. EntityTypeManager passed as service argument, not requested inside constructor.

---
+++ web/modules/custom/overwatch_analytics/src/CompetitiveSeasonsService.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Drupal\overwatch_analytics;
+
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\overwatch_match\Entity\OverwatchMatch;
+
+/**
+ * Class CompetitiveSeasonsService.
+ */
+class CompetitiveSeasonsService {
+
+  /**
+   * Season for which analyze will be done.
+   *
+   * @var \Drupal\overwatch_season\Entity\OverwatchSeason
+   */
+  protected $overwatchSeason;
+
+  /**
+   * User for which this analyze will be done.
+   *
+   * @var \Drupal\user\Entity\User
+   */
+  protected $account;
+
+  /**
+   * Result of analyze.
+   *
+   * @var array
+   *   Structured data with analyze.
+   */
+  protected $result;
+
+  /**
+   * Helper property to work with entities.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Matches for current account and season.
+   *
+   * @var array
+   *  An array with OverwatchMaches entites.
+   */
+  protected $matches;
+
+  /**
+   * Constructs a new CompetitiveSeasonsService object.
+   */
+  public function __construct(EntityTypeManager $entityTypeManager) {
+    $this->result = [];
+
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * Set OverwatchSeason.
+   *
+   * @param int $sid
+   *   OverwatchSeason entity id.
+   *
+   * @return \Drupal\overwatch_analytics\CompetitiveSeasonsService
+   */
+  public function setSeason($sid) {
+    $this->overwatchSeason = $this->entityTypeManager->getStorage('overwatch_season')
+      ->load($sid);
+    return $this;
+  }
+
+  /**
+   * Set account of user for which analyze will be done.
+   *
+   * @param int $uid
+   *   User entity id.
+   *
+   * @return \Drupal\overwatch_analytics\CompetitiveSeasonsService
+   */
+  public function setAccount($uid) {
+    $this->account = $this->entityTypeManager->getStorage('user')->load($uid);
+    return $this;
+  }
+
+  /**
+   * Return computed results.
+   *
+   * @return array|bool
+   *   An array with all results, if something wrong with account or season
+   *   returns FALSE.
+   */
+  public function getResult() {
+    if ($this->account && $this->overwatchSeason) {
+      if ($matches = $this->loadMatches()) {
+        $this->calculateSummary();
+        return $this->result;
+      }
+      return FALSE;
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+  /**
+   * Find and load all matches for current user and season.
+   *
+   * @return array
+   *   An array with matches. If no one found, returns empty array.
+   */
+  protected function loadMatches() {
+    if (empty($this->matches)) {
+      $this->matches = $this->entityTypeManager->getStorage('overwatch_match')
+        ->loadByProperties([
+          'user_id' => $this->account->id(),
+          'field_season' => $this->overwatchSeason->id(),
+        ]);
+    }
+    return $this->matches;
+  }
+
+  /**
+   * Calculate summary statistic.
+   */
+  protected function calculateSummary() {
+    $summary = [];
+    $summary['games_played'] = count($this->matches);
+
+    // Wins, losses and draws.
+    $summary['wins'] = 0;
+    $summary['draws'] = 0;
+    $summary['losses'] = 0;
+    /** @var \Drupal\overwatch_match\Entity\OverwatchMatch $match */
+    foreach ($this->matches as $match) {
+      if (!$match->field_match_result->isEmpty()) {
+        switch ($match->field_match_result->value) {
+          case OverwatchMatch::STATUS_VICTORY:
+            $summary['wins']++;
+            break;
+
+          case OverwatchMatch::STATUS_DRAW:
+            $summary['draws']++;
+            break;
+
+          case OverwatchMatch::STATUS_DEFEAT:
+            $summary['losses']++;
+        }
+      }
+    }
+
+    // WLD in percentage.
+    $summary['win_percentage'] = ($summary['wins'] * 100) / $summary['games_played'];
+    $summary['draw_percentage'] = ($summary['draws'] * 100) / $summary['games_played'];
+    $summary['losses_percentage'] = ($summary['losses'] * 100) / $summary['games_played'];
+
+    $this->result['summary'] = $summary;
+  }
+
+}
--- web/modules/custom/overwatch_match/src/OverwatchMatchHelperService.php
+++ web/modules/custom/overwatch_match/src/OverwatchMatchHelperService.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\overwatch_match;
 
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\overwatch_match\Entity\OverwatchMatch;
+
 /**
  * Class OverwatchMatchHelperService.
  */
@@ -12,9 +15,8 @@ class OverwatchMatchHelperService {
   /**
    * Constructs a new OverwatchMatchHelperService object.
    */
-  public function __construct() {
-    $this->entityTypeManager = \Drupal::entityTypeManager()
-      ->getStorage('overwatch_match');
+  public function __construct(EntityTypeManager $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
   }
 
   /**
@@ -47,6 +49,54 @@ class OverwatchMatchHelperService {
       ->condition('user_id', $uid)
       ->range(0, 1);
     return (bool) $query->count()->execute();
+  }
+
+  /**
+   * Find previous match for user and season.
+   */
+  public function getPreviousMatch($match_id) {
+    $overwatch_match = $this->entityTypeManager->getStorage('overwatch_match')
+      ->load($match_id);
+    if ($overwatch_match) {
+      if (!$overwatch_match->field_season->isEmpty()) {
+        $season_id = $overwatch_match->field_season->target_id;
+        $query = \Drupal::entityQuery('overwatch_match')
+          ->condition('field_season.target_id', $season_id)
+          ->condition('created', $overwatch_match->created->value, '<=')
+          ->condition('user_id', $overwatch_match->user_id->target_id)
+          ->condition('id', $overwatch_match->id(), '<>')
+          ->range(0, 1)
+          ->sort('created', 'DESC');
+
+        $entities = $query->execute();
+        return $entities ? reset($entities) : FALSE;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * Trying to set status for match based on previous matches.
+   *
+   * This method only update value for filed, but not save an entity.
+   */
+  public function updateMatchStatus(OverwatchMatch $match) {
+    $previous_match_id = $this->getPreviousMatch($match->id());
+    if ($previous_match_id) {
+      $previous_match = $this->entityTypeManager->getStorage('overwatch_match')
+        ->load($previous_match_id);
+      $previous_sr = $previous_match->field_skill_rating->value;
+      $current_sr = $match->field_skill_rating->value;
+      if ($previous_sr == $current_sr) {
+        $match->field_match_result = OverwatchMatch::STATUS_DRAW;
+      }
+      elseif ($previous_sr < $current_sr) {
+        $match->field_match_result = OverwatchMatch::STATUS_VICTORY;
+      }
+      else {
+        $match->field_match_result = OverwatchMatch::STATUS_DEFEAT;
+      }
+    }
   }
 
 }
