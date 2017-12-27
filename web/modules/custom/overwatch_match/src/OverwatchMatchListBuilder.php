6a4c5a55cf74eeeced7c5065fb3dbf167d8d70d8
Niklan <niklanrus@gmail.com>
2017-12-27 10:52:53 +0500

    - OverwatchMatch list of entities is now sorted by DESC. - Changed table columns for OverwatchMatch listing. Removed name which is pointless here, added player name. Match ID is now link to it match page.

--- web/modules/custom/overwatch_match/src/OverwatchMatchListBuilder.php
+++ web/modules/custom/overwatch_match/src/OverwatchMatchListBuilder.php
@@ -13,13 +13,29 @@ use Drupal\Core\Link;
  */
 class OverwatchMatchListBuilder extends EntityListBuilder {
 
+  /**
+   * Loads entity IDs using a pager sorted by the entity id.
+   *
+   * @return array
+   *   An array of entity IDs.
+   */
+  protected function getEntityIds() {
+    $query = $this->getStorage()->getQuery()
+      ->sort($this->entityType->getKey('id'), 'DESC');
+
+    // Only add the pager if a limit is specified.
+    if ($this->limit) {
+      $query->pager($this->limit);
+    }
+    return $query->execute();
+  }
 
   /**
    * {@inheritdoc}
    */
   public function buildHeader() {
-    $header['id'] = $this->t('Overwatch match ID');
-    $header['name'] = $this->t('Name');
+    $header['id'] = 'MID';
+    $header['player'] = $this->t('Player');
     return $header + parent::buildHeader();
   }
 
@@ -28,12 +44,12 @@ class OverwatchMatchListBuilder extends EntityListBuilder {
    */
   public function buildRow(EntityInterface $entity) {
     /* @var $entity \Drupal\overwatch_match\Entity\OverwatchMatch */
-    $row['id'] = $entity->id();
-    $row['name'] = Link::createFromRoute(
-      $entity->label(),
+    $row['id'] = Link::createFromRoute(
+      $entity->id(),
       'entity.overwatch_match.edit_form',
       ['overwatch_match' => $entity->id()]
     );
+    $row['player'] = $entity->user_id->entity->label();
     return $row + parent::buildRow($entity);
   }
 
